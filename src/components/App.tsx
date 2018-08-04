348eba024f4604a2678c17ec7250c0910f681f83
Aidan Heller <asfheller@gmail.com>
2018-08-03 20:35:21 -0700

    Add app component

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import { h } from 'preact';
+
+export const App = () => (
+    <div id="app">
+        <h1>Here is my app!</h1>
+    </div>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import { h, render } from 'preact';
 import './style/index.scss';
 
-render(<h1 id="root">It's TypeScript time!</h1>, document.body, document.getElementById('root'));
+import { App } from './components/App'
+
+render(<App />, document.body, document.getElementById('root'));
